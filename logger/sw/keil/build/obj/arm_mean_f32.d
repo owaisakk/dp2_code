c12b897ee111bb6a99e45a2402d7d0949b961143
Owais Khan <owaisakk@gmail.com>
2013-03-18 22:00:24 -0400

    Revert "Added modified logger"
    This reverts commit 5db820891be46265f2137fbb49fc6687a215add0.

--- logger/sw/keil/build/obj/arm_mean_f32.d
+++
@@ -1,8 +0,0 @@
-.\build\obj\arm_mean_f32.o: ..\CMSIS\DSP_Lib\Source\StatisticsFunctions\arm_mean_f32.c
-.\build\obj\arm_mean_f32.o: ..\CMSIS\Include\arm_math.h
-.\build\obj\arm_mean_f32.o: ..\CMSIS\Include\core_cm3.h
-.\build\obj\arm_mean_f32.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\build\obj\arm_mean_f32.o: ..\CMSIS\Include\core_cmInstr.h
-.\build\obj\arm_mean_f32.o: ..\CMSIS\Include\core_cmFunc.h
-.\build\obj\arm_mean_f32.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
-.\build\obj\arm_mean_f32.o: C:\Keil\ARM\ARMCC\bin\..\include\math.h
--- logger/sw/keil/build/obj/ff.d
+++
@@ -1,5 +0,0 @@
-.\build\obj\ff.o: ..\fatfs\src\ff.c
-.\build\obj\ff.o: ..\fatfs\src\ff.h
-.\build\obj\ff.o: ..\fatfs\src\integer.h
-.\build\obj\ff.o: ..\fatfs\src\ffconf.h
-.\build\obj\ff.o: ..\fatfs\src\diskio.h
